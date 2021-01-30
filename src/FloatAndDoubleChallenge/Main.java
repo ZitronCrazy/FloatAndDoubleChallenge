b97e9c1a8ea829ef41ed44066ea86bb3d82393ea
ZitronCrazy <zitro5@protonmail.com>
2021-01-30 23:52:00 +0100

    FloatAndDoubleChallenge

---
+++ src/FloatAndDoubleChallenge/Main.java
@@ -0,0 +1,12 @@
+package FloatAndDoubleChallenge;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    double Pounds = 100d;
+    double InKg = Pounds * 0.45359237;
+    System.out.println("converted into " + InKg);
+
+    }
+}
